20f6bc9b2c9c44a250a4b45bf7c380477761cc15
EC2 Default User <ec2-user@ip-172-31-36-152.ap-northeast-1.compute.internal>
2023-03-28 16:01:45 +0000

    課題作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def new
+    
+  end
+
+  
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to '/books'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.all
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  
+  # 投稿データのストロングパラメータ
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+end
---
+++ app/models/book.rb
@@ -0,0 +1,20 @@
+class Book < ApplicationRecord
+    has_one_attached :image
+  belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 200 }
+  
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+end
